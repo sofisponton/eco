5a026a5ad23bec4baddd0a567b6853c8cad3768c
Sponton <134696651+sofisponton@users.noreply.github.com>
2024-04-03 22:39:02 +0000

    commit ai

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import {NavigationContainer} from '@react-navigation/native';
 import fundo from './assets/fundo.png';
 import titulo from './assets/logo.png';
+import eco from './assets/eco-title.png';
 
 function HomeScreen({navigation}){
   return(
@@ -21,7 +22,7 @@ function HomeScreen({navigation}){
             marginTop: 65,
             height: 60,
         }} onPress={()=>navigation.navigate("Ecologia")}>
-        <Text> Ecologia</Text>
+        <Text style={{color: '#F9FDED', fontWeight: 'bold',}}> Ecologia</Text>
       </Pressable>
       <Pressable style={{flexDirection: 'row',
             alignItems: 'center',
@@ -34,7 +35,7 @@ function HomeScreen({navigation}){
             marginTop: 35,
             height: 60,
         }} onPress={()=>navigation.navigate("Insetos")}>
-        <Text>  Insetos  </Text>
+        <Text style={{color: '#F9FDED', fontWeight: 'bold',}}>  Insetos  </Text>
       </Pressable>
       <Pressable style={{flexDirection: 'row',
             alignItems: 'center',
@@ -47,7 +48,7 @@ function HomeScreen({navigation}){
             marginTop: 35,
             height: 60,
         }} onPress={()=>navigation.navigate("About")}>
-        <Text>   Sobre   </Text>
+        <Text style={{color: '#F9FDED', fontWeight: 'bold',}}>   Sobre   </Text>
       </Pressable>
       
     </View>
@@ -59,12 +60,28 @@ function HomeScreen({navigation}){
 
 function Ecologia({navigation}) {
   return (
+    <ImageBackground source={fundo} resizeMode='cover' style={{flex: 1,justifyContent: 'center'}}>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tela de Ecologia</Text>
-      <TouchableOpacity onPress={()=>navigation.goBack()}>
+    <Image source={eco} style={{ width: 300, height: 200, }} />
+          <Text style={{ padding: 20, }}>A ecologia é a ciência que estuda as interações entre os organismos e seu ambiente. Ela examina como os seres vivos estão distribuídos e interagem entre si e com seu ambiente físico e químico. Isso envolve a análise de diferentes níveis de organização, desde populações individuais até ecossistemas e a biosfera como um todo.
+               Dentro da ecologia, são investigados os padrões e processos que influenciam as populações e comunidades de organismos. Isso inclui o estudo da dinâmica populacional, distribuição geográfica, diversidade de espécies e interações entre elas, como competição, predação e mutualismo.
+               A ecologia desempenha um papel fundamental na compreensão e na conservação dos sistemas naturais. Ao estudar como os seres vivos interagem entre si e com seu ambiente, podemos desenvolver estratégias para a gestão sustentável dos recursos naturais e para a proteção da biodiversidade em nosso planeta.
+          </Text>
+      <Pressable style={{flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingVertical: 12,
+            paddingHorizontal: 42,
+            borderRadius: 20,
+            elevation: 3,
+            backgroundColor: '#4A8D43',
+            marginTop: 65,
+            height: 60,
+        }} onPress={()=>navigation.goBack()}>
         <Text>Voltar</Text>
-      </TouchableOpacity>
+      </Pressable>
     </View>
+    </ImageBackground>
   );
 }
 
