fb91b116140077630d46a4696246736031eba756
Sponton <134696651+sofisponton@users.noreply.github.com>
2024-04-06 22:47:15 +0000

    eco commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import {NavigationContainer} from '@react-navigation/native';
 import fundo from './assets/fundo.png';
 import titulo from './assets/logo.png';
-import eco from './assets/eco-title.png';
+import eco from './assets/eco-titulo.png';
+import insetos from './assets/insetos-titulo.png';
+import sobre from './assets/sobre-titulo.png';
 
 function HomeScreen({navigation}){
   return(
@@ -78,7 +80,7 @@ function Ecologia({navigation}) {
             marginTop: 65,
             height: 60,
         }} onPress={()=>navigation.goBack()}>
-        <Text>Voltar</Text>
+        <Text  style={{color: '#F9FDED', fontWeight: 'bold'}}>Voltar</Text>
       </Pressable>
     </View>
     </ImageBackground>
@@ -87,23 +89,53 @@ function Ecologia({navigation}) {
 
 function Insetos({navigation}) {
   return (
+    <ImageBackground source={fundo} resizeMode='cover' style={{flex: 1,justifyContent: 'center'}}>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tela de Insetos</Text>
-      <TouchableOpacity onPress={()=>navigation.goBack()}>
-        <Text>Voltar</Text>
-      </TouchableOpacity>
+    <Image source={insetos} style={{ width: 300, height: 200, }} />
+          <Text style={{ padding: 20, }}>Os insetos desempenham um papel crucial na ecologia como polinizadores, decompositores e reguladores de populações. Como polinizadores, são responsáveis pela reprodução de muitas plantas, incluindo várias culturas agrícolas, contribuindo para a produção de alimentos. Além disso, os insetos desempenham um papel importante na decomposição de matéria orgânica, ajudando a reciclar nutrientes e a manter a fertilidade do solo. Eles também atuam como reguladores de populações, controlando as populações de outros organismos, como plantas e outros insetos, e servindo como alimento para uma variedade de predadores.
+
+A diversidade de insetos é um indicador da saúde ecológica de um ecossistema. Ecossistemas saudáveis geralmente possuem uma grande variedade de insetos, cada um desempenhando um papel específico na teia alimentar e nas interações ecológicas. A perda de diversidade de insetos pode ter efeitos cascatares em toda a cadeia alimentar e na estabilidade do ecossistema, afetando a disponibilidade de alimentos para outros organismos e reduzindo a resistência a perturbações ambientais.
+
+Além disso, os insetos têm sido fonte de inspiração para inúmeras descobertas científicas e aplicações tecnológicas, desde a produção de seda até o desenvolvimento de novos medicamentos. Portanto, entender e conservar a diversidade de insetos é essencial para a manutenção da biodiversidade global e para o desenvolvimento sustentável de nossas sociedades.</Text>
+      <Pressable style={{flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingVertical: 12,
+            paddingHorizontal: 42,
+            borderRadius: 20,
+            elevation: 3,
+            backgroundColor: '#4A8D43',
+            marginTop: 65,
+            height: 60,
+        }} onPress={()=>navigation.goBack()}>
+        <Text  style={{color: '#F9FDED', fontWeight: 'bold'}}>Voltar</Text>
+      </Pressable>
     </View>
+    </ImageBackground>
   );
 }
 
 function AboutScreen({navigation}) {
   return (
+    <ImageBackground source={fundo} resizeMode='cover' style={{flex: 1,justifyContent: 'center'}}>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tela de sobre</Text>
-      <TouchableOpacity onPress={()=>navigation.goBack()}>
-        <Text>Voltar</Text>
-      </TouchableOpacity>
+    <Image source={sobre} style={{ width: 300, height: 200, }} />
+        
+      <Pressable style={{flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingVertical: 12,
+            paddingHorizontal: 42,
+            borderRadius: 20,
+            elevation: 3,
+            backgroundColor: '#4A8D43',
+            marginTop: 65,
+            height: 60,
+        }} onPress={()=>navigation.goBack()}>
+        <Text  style={{color: '#F9FDED', fontWeight: 'bold'}}>Voltar</Text>
+      </Pressable>
     </View>
+    </ImageBackground>
   );
 }
 
